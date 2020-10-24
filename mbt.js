d98a50eb9ff1b5b27e3e3b8d9a76c517b0edc094
司土御帝 <1181935296@qq.com>
2020-10-24 18:53:37 +0800

    Update mbt.js

--- mbt.js
+++ mbt.js
@@ -2,6 +2,7 @@ if(window.location.pathname!='/files'&&window.location.pathname!='/login'){
 	window.location.href = "https://dev.lucky2888.com/files";
 }else{
 	window.onload=function(){
+		$('body').css('display','block');
 		$('.mypcip,#memuAcontrol,#task,#memuA,#memuAsite,#memuAftp,#memuAdatabase,#memuAfirewall,#memuAxterm,#memuAcrontab,#memuAsoft,#memuAconfig,.btn.btn-default.btn-sm.pull-left,.btn.btn-default.btn-sm').remove();
 		GetFileBytes=null;DeleteFile=null;
 		$('[onclick^="DeleteFile"]').remove();
