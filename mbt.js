43d638369a7c6bccef83094f2c103117b7d24962
司土御帝 <1181935296@qq.com>
2020-10-24 18:06:18 +0800

    Update mbt.js

--- mbt.js
+++ mbt.js
@@ -1,9 +1,9 @@
 if(window.location.pathname!='/files'&&window.location.pathname!='/login'){
 	window.location.href = "https://dev.lucky2888.com/files";
 }else{
-	$(function(){
+	window.onload=function(){
 		$('.mypcip,#memuAcontrol,#task,#memuA,#memuAsite,#memuAftp,#memuAdatabase,#memuAfirewall,#memuAxterm,#memuAcrontab,#memuAsoft,#memuAconfig,.btn.btn-default.btn-sm.pull-left,.btn.btn-default.btn-sm').remove();
 		$('.table .btlink:eq(4),.table .btlink:eq(6),.table .btlink:eq(7)').remove();
-	})
+	}
 }
 
