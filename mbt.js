4fca44c3b86eaa534466d07ff199f9c60b6f8273
司土御帝 <1181935296@qq.com>
2020-10-24 20:00:57 +0800

    Update mbt.js

--- mbt.js
+++ mbt.js
@@ -2,7 +2,7 @@ if(window.location.pathname!='/files'&&window.location.pathname!='/login'){
 	window.location.href = "https://dev.lucky2888.com/files";
 }else{
 	function init(){
-		if(!$) return;
+		if(!jQuery) return;
 		$('body').css('display','block');
 		$('.mypcip,#memuAcontrol,#task,#memuA,#memuAsite,#memuAftp,#memuAdatabase,#memuAfirewall,#memuAxterm,#memuAcrontab,#memuAsoft,#memuAconfig').remove();
 		$('[onclick^="DeleteFile"]').remove();
