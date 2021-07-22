401e8b5d8c7b30e14dd2e463c9b61ef455c0f1d7
司土御帝 <1181935296@qq.com>
2021-07-22 16:05:56 +0800

    Update qyxc.js

--- qyxc.js
+++ qyxc.js
@@ -1,8 +1,8 @@
-window.onload=function(){
-  setInterval(function(){
-    if($('#sitetips,#fk_faiVisitStateAd').length){
-        $('#sitetips,#fk_faiVisitStateAd').remove();
-    }
-  },500);
+$(function(){
   $('.s_footer p').eq(1).remove();
-}
+})
+setInterval(function(){
+  if($('#sitetips,#fk_faiVisitStateAd').length){
+      $('#sitetips,#fk_faiVisitStateAd').remove();
+  }
+},500);
