820c3b5819bae2e6d3177241faaf6594b94ae0c0
司土御帝 <1181935296@qq.com>
2021-07-22 16:09:44 +0800

    Update qyxc.js

--- qyxc.js
+++ qyxc.js
@@ -1,8 +1,9 @@
 $(function(){
   $('.s_footer p').eq(1).remove();
 })
-setInterval(function(){
+
+var aaa=setInterval(function(){
   if($('#sitetips,#fk_faiVisitStateAd').length){
       $('#sitetips,#fk_faiVisitStateAd').remove();
   }
-},500);
+},200);
