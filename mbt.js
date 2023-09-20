e364c3554a2039b8d743e8c85b87d197f532e6dd
biluweb <1181935296@qq.com>
2023-09-20 11:44:58 +0800

    sn

--- mbt.js
+++ mbt.js
@@ -1,3 +1,3 @@
 $(function(){
-	$("img[src='https://qiniu-pub.appbsl.cn/qrcode/image/4e8e3a70c8f417e1e72259f04daf8546.png']").attr("src",window.location.href);
+	$("img[src='https://qiniu-pub.appbsl.cn/qrcode/image/4e8e3a70c8f417e1e72259f04daf8546.png']").attr("src","https://biluweb.github.io/assets/lucky2888/resources/images/mobile_qrcode.png");
 })
