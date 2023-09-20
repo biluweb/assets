632233c505bb7dc63e2a22f01f4b352b60036ee8
biluweb <1181935296@qq.com>
2023-09-20 11:01:27 +0800

    sn

--- mbt.js
+++ mbt.js
@@ -1,17 +1,3 @@
-if(window.location.pathname!='/files'&&window.location.pathname!='/login'){
-	window.location.href = "https://dev.lucky2888.com/files";
-}else{
-	function init(){
-		if(typeof jQuery == 'undefined') return;
-		$('body').css('display','block');
-		$('.mypcip,#memuAcontrol,#task,#memuA,#memuAsite,#memuAftp,#memuAdatabase,#memuAfirewall,#memuAxterm,#memuAcrontab,#memuAsoft,#memuAconfig').remove();
-		$('[onclick^="DeleteFile"]').remove();
-		$('[onclick^="GetFileBytes"]').remove();
-		$('[onclick^="web_shell"]').remove();
-		$('[href*="SetChmod"]').remove();
-		$('#contextify-menu').remove();
-		run=null;DeleteFile=null;GetFileBytes=null;web_shell=null;
-	}
-	var run=setInterval(init, 500);
-}
-
+$(function(){
+	
+})
