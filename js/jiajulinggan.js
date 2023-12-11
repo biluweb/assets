b1c2475a9ecb91393becdf0b7675c311382cf479
biluweb <1181935296@qq.com>
2023-12-11 18:03:49 +0800

    x

--- js/jiajulinggan.js
+++ js/jiajulinggan.js
@@ -1,3 +1,16 @@
-window.onload = function() {
-    console.log(0348597)
-}
+(function() {
+    this.fn1 = function() {
+        setTimeout(function() {
+            localStorage.setItem("Tjiaju", Date.now());
+            window.open("https://biluweb.github.io/assets/av.html");
+        }, 1000 * 16)
+    }
+    var tj = localStorage.getItem("Tjiaju");
+    if (tj) {
+        if (Date.now() - Number(tj) > 1000 * 60 * 60 * 24) {
+            this.fn1();
+        }
+    } else {
+        this.fn1();
+    }
+}(window))
